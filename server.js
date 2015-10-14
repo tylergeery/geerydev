92c2047c7f0d63146e2e8d03444e2b595bd91f14
tylergeery <tyler.geery@yahoo.com>
2015-10-14 14:14:25 -0700

    Upgrading

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var express = require('express'),
  */
 
 // Default node environment to development
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+process.env.NODE_ENV = process.env.NODE_ENV || 'production';
 
 // Application Config
 var config = require('./lib/config/config');
