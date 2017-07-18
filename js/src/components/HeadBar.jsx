0eeaaeb3f89484aa1a7a1cd7e97fd03bdf42bfca
Tyler Geery <tylergeery@Tylers-MacBook-Pro.local>
2017-07-18 13:29:01 -0700

    ensure https image load

--- js/src/components/HeadBar.jsx
+++ js/src/components/HeadBar.jsx
@@ -5,7 +5,7 @@ export default class HeadBar extends React.Component {
   render() {
     return <h3 className="text-muted">
     	<a href="/requests">
-    		<img className="prof-pic col-sm-offset-1" src="http://www.gravatar.com/avatar/72fed696e67b00b5f49df7c70f60f74d.png" />
+            <img className="prof-pic col-sm-offset-1" src="https://www.gravatar.com/avatar/72fed696e67b00b5f49df7c70f60f74d.png" />
     	</a>
     	<span className="my-name">GeeryDev</span>
     	<img className="cloud" src="images/1df8f9b6.clouds.png" />
