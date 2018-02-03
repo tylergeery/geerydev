a647ae444d52abb0f4417a5373bbd1e9809c37f3
tylergeery <tyler.geery@yahoo.com>
2018-02-03 09:04:38 -0800

    start saving keywords smarter

--- scrapey/refreshKeywords.js
+++ scrapey/refreshKeywords.js
@@ -1,23 +1,46 @@
 var config = require('../lib/config/config'),
     mongoose = require('mongoose'),
     db = mongoose.connect(config.mongo.uri, config.mongo.options),
+    middleware = require('../lib/middleware'),
     TIBW = require('../lib/models/tibw'),
-    Blog = require('../lib/models/blog');
+    Blog = require('../lib/models/blog'),
+    blogsCount = 0, blogsDone = false,
+    tibwCount = 0, tibwDone = false;
 
 Blog.find({})
-    .limit(1) // temporarily limit
+    .limit(100)
     .exec(function (err, blogs) {
         blogs.forEach(function (blog) {
-            console.log('blog:', blog);
+            blog.keywords = middleware.extractKeywords(blog.question + ' ' + blog.response);
             blog.save();
+            blogsCount++;
+
+            if (blogsCount === blogs.length) {
+                blogsDone = true;
+            }
         });
     });
 
 TIBW.find({})
-    .limit(1) // temporarily limit
+    .limit(100)
     .exec(function (err, tibws) {
         tibws.forEach(function (tibw) {
-            console.log('tibw:', tibw);
+            tibw.keywords = middleware.extractKeywords(tibw.title + ' ' + tibw.content);
             tibw.save();
+            tibwCount++;
+
+            if (tibwCount === tibws.length) {
+                tibwDone = true;
+            }
         });
     });
+
+function wait() {
+    if (tibwDone && blogsDone) {
+        mongoose.disconnect();
+    } else {
+        setTimeout(wait, 200);
+    }
+};
+
+wait();
