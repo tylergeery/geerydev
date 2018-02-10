2a89ed37b62c8abd2d9b8186a271020358dabc57
tylergeery <tyler.geery@yahoo.com>
2018-02-10 07:09:36 -0800

    react render server side

--- js/src/common/navComponents.js
+++ js/src/common/navComponents.js
@@ -1,25 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
 
 import store from '../store';
 import GeeryDevHeadBar from '../containers/GeeryDevHeadBar';
 import GeeryDevNavBar from '../containers/GeeryDevNavBar';
 import headbarActions from '../actions/headbar';
 
-ReactDOM.render(
-    <Provider store={store}>
-        <GeeryDevHeadBar />
-    </Provider>,
-  document.getElementById('geerydev-header')
-);
+// ReactDOM.render(
+//     <Provider store={store}>
+//         <GeeryDevHeadBar />
+//     </Provider>,
+//   document.getElementById('geerydev-header')
+// );
 
-ReactDOM.render(
-    <Provider store={store}>
-        <GeeryDevNavBar />
-    </Provider>,
-  document.getElementById('geerydev-navbar')
-);
+// ReactDOM.render(
+//     <Provider store={store}>
+//         <GeeryDevNavBar />
+//     </Provider>,
+//   document.getElementById('geerydev-navbar')
+// );
 
 let ts = window.performance.now();
 let step = function (current) {
