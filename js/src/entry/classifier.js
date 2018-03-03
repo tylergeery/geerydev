2225214f0c120f1106e2dbbdbe5b6a2ff2db17f4
tylergeery <tyler.geery@yahoo.com>
2018-03-03 09:17:34 -0800

    start classifier page and 404 status to errors

---
+++ js/src/entry/classifier.js
@@ -0,0 +1,15 @@
+import 'whatwg-fetch';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
+import '../common/navComponents';
+import store from '../store';
+import GeeryDevClassifier from '../containers/GeeryDevClassifier';
+
+ReactDOM.render(
+    <Provider store={store}>
+        <GeeryDevClassifier />
+    </Provider>,
+  document.getElementById('geerydev-classifier')
+);
