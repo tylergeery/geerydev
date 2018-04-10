6b4226c1836d1481396f10446c25a7e90961fbed
tylergeery <tyler.geery@yahoo.com>
2018-04-09 22:47:59 -0700

    fix path to posts state

--- js/src/containers/admin/AdminPosts.jsx
+++ js/src/containers/admin/AdminPosts.jsx
@@ -1,25 +1,25 @@
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
 
-import store from '../../store'
-import Posts from '../../components/admin/posts'
-import postActions from '../../actions/post'
+import store from '../../store';
+import Posts from '../../components/admin/posts';
+import postActions from '../../actions/post';
 
-const mapStateToProps = (state, ownProps) => {
-    return {
-        posts: state.posts || []
+const mapStateToProps = (state, ownProps) => (
+    {
+        posts: state.posts.list || []
     }
-}
+);
 
-const mapDispatchToProps = (dispatch, ownProps) => {
-    return {
-        submitAction: function(blogId, event) {
+const mapDispatchToProps = (dispatch, ownProps) => (
+    {
+        submitAction(blogId, event) {
             if (this.state.toggle !== 'add') {
-                event.stopPropagation()
+                event.stopPropagation();
 
                 store.dispatch(postActions.remove(blogId))
                     .then(() => {
-                        store.dispatch(postActions.getPostList('created', '_id'))
-                    })
+                        store.dispatch(postActions.getPostList('created', '_id'));
+                    });
             }
         },
 
@@ -33,21 +33,21 @@ const mapDispatchToProps = (dispatch, ownProps) => {
             this.setState({
                 showPostForm: true,
                 post: post
-            })
-            window.scrollTo(0, 0)
+            });
+            window.scrollTo(0, 0);
         },
 
         toggleAddDelete() {
             this.setState({
                 toggle: this.state.toggle === 'add' ? 'delete' : 'add'
-            })
+            });
         }
     }
-}
+);
 
 const AdminPosts = connect(
     mapStateToProps,
     mapDispatchToProps
-)(Posts)
+)(Posts);
 
-export default AdminPosts
+export default AdminPosts;
