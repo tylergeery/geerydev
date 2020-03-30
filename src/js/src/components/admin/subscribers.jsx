4e042f4b3f777a1efc45ce1f2431e981b2d477b9
tylergeery <tyler.geery@yahoo.com>
2020-03-29 22:00:33 -0700

    fix for now

--- src/js/src/components/admin/subscribers.jsx
+++ src/js/src/components/admin/subscribers.jsx
@@ -19,15 +19,15 @@ export default class Posts extends React.Component {
                     <tr>
             			<th className="pad-box">Email</th>
             			<th className="pad-box">Date</th>
-                        <th className="pad-box">Delete</th>
+                        <th className="pad-box">Preferences</th>
             		</tr>
-                    {this.props.subscribers.map(function(s) {
-                        return <tr key={s._id}>
+                    {this.props.subscribers.map((s) => (
+                        <tr key={s._id}>
                 			<td className="pad-box">{s.email}</td>
                 			<td className="pad-box">{time.iso8601ToFullReadable(s.created)}</td>
                             <td className="red-delete center point" onClick={this.props.deleteSubscriber.bind(this, s._id)}>-</td>
                 		</tr>
-                    })}
+                    ))}
                 </tbody>
         	</table>
         </div>
